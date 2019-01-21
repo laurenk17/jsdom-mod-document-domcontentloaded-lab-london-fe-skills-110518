edbb26218d5345644de7b7352ea829eb3424f353
lauren kernan <lauren.kernan17@gmail.com>
2019-01-21 18:30:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 document.addEventListener('DOMContentLoaded', () =>
   {
     let e = document.querySelector("div#hidden-div");
-    let newVal = parseInt(e.inner)
+    let newVal = parseInt(e.innerText)
     })
